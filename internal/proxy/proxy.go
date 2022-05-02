36921dbf93d248f55fa24dd0d3eaec06909660e6
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-02 14:06:20 +0430

    origin server forwarding in proxy added

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -4,13 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 // New : creates a new reverse proxy server on port 8080
 func New() {
+	// forward client to the main server
+	originServerURL, err := url.Parse("http://127.0.0.1:8081")
+	if err != nil {
+		log.Fatal("invalid origin server URL")
+	}
+
 	reverseProxy := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
+
+		// set the parameters to forward our client to the main server
+		req.Host = originServerURL.Host
+		req.URL.Host = originServerURL.Host
+		req.URL.Scheme = originServerURL.Scheme
+		req.RequestURI = ""
+
+		// send a request to the origin server
+		_, err := http.DefaultClient.Do(req)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+
+			_, _ = fmt.Fprint(rw, err)
+
+			return
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", reverseProxy))
