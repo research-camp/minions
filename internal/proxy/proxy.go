9fb25ddaa722f509b668514759159673c1306689
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-02 13:59:32 +0430

    proxy server init

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,1 +1,17 @@
 package proxy
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// New : creates a new reverse proxy server on port 8080
+func New() {
+	reverseProxy := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", reverseProxy))
+}
