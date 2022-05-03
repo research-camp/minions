0d02037e4bb0ac070da865dc7b4853648c4ce68c
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 19:16:01 +0430

    proxy server refactor

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"github.com/amirhnajafiz/xerox/internal/config"
+	"github.com/amirhnajafiz/xerox/internal/metric"
 	"github.com/amirhnajafiz/xerox/proxy"
 	"github.com/amirhnajafiz/xerox/server"
 )
 
 func main() {
+	// loading configs
 	cfg := config.Load()
 
+	if cfg.Metric.Enable {
+		metric.NewServer(cfg.Metric).Start()
+	}
+
 	// starting a proxy server
-	go proxy.New(cfg.Proxy, cfg.Metric)
+	go proxy.New(cfg.Proxy)
 
 	// starting a new server
 	server.New()
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -9,17 +9,14 @@ import (
 )
 
 // New : creates a new reverse proxy server on port 8080
-func New(cfg Config, mCfg metric.Config) {
-	if mCfg.Enable {
-		metric.NewServer(mCfg).Start()
-	}
-
+func New(cfg Config) {
 	// forward client to the main server
 	originServerURL, err := url.Parse(cfg.BaseURL)
 	if err != nil {
 		log.Fatal("invalid origin server URL")
 	}
 
+	// reverse proxy server initialize
 	reverseProxy := http.HandlerFunc(HandleRequest(originServerURL, metric.NewMetrics()))
 
 	log.Fatal(http.ListenAndServe(cfg.Address, reverseProxy))
