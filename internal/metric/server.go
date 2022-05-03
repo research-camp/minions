e2eacd04e94d370ffc1f2f1dc2461f456ae33380
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 10:11:33 +0430

    server for metrics implemented

--- internal/metric/server.go
+++ internal/metric/server.go
@@ -1,1 +1,40 @@
 package metric
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Server struct {
+	Srv     *http.ServeMux
+	Address string
+}
+
+// NewServer creates a new monitoring server.
+func NewServer(cfg Config) Server {
+	var srv *http.ServeMux
+
+	if cfg.Enable {
+		srv = http.NewServeMux()
+		srv.Handle("/metrics", promhttp.Handler())
+	}
+
+	return Server{
+		Address: cfg.Host,
+		Srv:     srv,
+	}
+}
+
+// Start creates and run a metric server for prometheus in new go routine.
+func (s Server) Start() {
+	if s.Srv == nil {
+		return
+	}
+
+	go func() {
+		if err := http.ListenAndServe(s.Address, s.Srv); err != nil {
+			panic(err)
+		}
+	}()
+}
