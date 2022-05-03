75df08362eb5aeb12f6ec233f88f3b8cb17d4069
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 09:59:55 +0430

    config implemented

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,37 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
+type Config struct {
+}
+
+func Load() Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		_ = fmt.Errorf("error loading deafult: %v\n", err)
+	}
+
+	// load configs file
+	if err := k.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		_ = fmt.Errorf("error loading config.yaml file: %v\n", err)
+	}
+
+	// unmarshalling
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %v\n", err)
+	}
+
+	return instance
+}
