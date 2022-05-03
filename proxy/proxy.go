eff1203dbb7acd48ea5970a9570717f95b795359
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 09:05:38 +0430

    handler proxy added

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,12 +1,9 @@
 package proxy
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"time"
 )
 
 // New : creates a new reverse proxy server on port 8080
@@ -17,29 +14,7 @@ func New() {
 		log.Fatal("invalid origin server URL")
 	}
 
-	reverseProxy := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
-
-		// set the parameters to forward our client to the main server
-		req.Host = originServerURL.Host
-		req.URL.Host = originServerURL.Host
-		req.URL.Scheme = originServerURL.Scheme
-		req.RequestURI = ""
-
-		// send a request to the origin server
-		originServerResponse, err := http.DefaultClient.Do(req)
-		if err != nil {
-			rw.WriteHeader(http.StatusInternalServerError)
-
-			_, _ = fmt.Fprint(rw, err)
-
-			return
-		}
-
-		// return response to the client
-		rw.WriteHeader(http.StatusOK)
-		_, _ = io.Copy(rw, originServerResponse.Body)
-	})
+	reverseProxy := http.HandlerFunc(HandleRequest(originServerURL))
 
 	log.Fatal(http.ListenAndServe(":8080", reverseProxy))
 }
