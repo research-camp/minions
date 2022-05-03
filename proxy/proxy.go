199f33f759a3e3626453c3dd691ce3288b49f2ba
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 19:04:19 +0430

    metrics and config init

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -4,10 +4,18 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/amirhnajafiz/xerox/internal/config"
+	"github.com/amirhnajafiz/xerox/internal/metric"
 )
 
 // New : creates a new reverse proxy server on port 8080
 func New() {
+	cfg := config.Load()
+	mtc := metric.NewMetrics()
+
+	metric.NewServer(cfg.Metric).Start()
+
 	// forward client to the main server
 	originServerURL, err := url.Parse("http://127.0.0.1:8081")
 	if err != nil {
