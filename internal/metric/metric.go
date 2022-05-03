e7cc9a510d17d3d210339d9b4cf2ed045ac12015
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 10:12:58 +0430

    metric init

--- internal/metric/metric.go
+++ internal/metric/metric.go
@@ -1,1 +1,15 @@
 package metric
+
+import "github.com/prometheus/client_golang/prometheus"
+
+const (
+	Namespace = "xerox"
+	Subsystem = "proxy"
+)
+
+// Metrics has all the client metrics.
+type Metrics struct {
+	TotalRequests      prometheus.Counter
+	SuccessfulRequests prometheus.Counter
+	FailedRequests     prometheus.Counter
+}
