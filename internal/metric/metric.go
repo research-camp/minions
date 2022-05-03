2652b20ddba2ae0714fb7155c5a824d1448c438f
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 10:15:22 +0430

    metrics implemented

--- internal/metric/metric.go
+++ internal/metric/metric.go
@@ -13,3 +13,39 @@ type Metrics struct {
 	SuccessfulRequests prometheus.Counter
 	FailedRequests     prometheus.Counter
 }
+
+func newCounter(counterOpts prometheus.CounterOpts) prometheus.Counter {
+	ev := prometheus.NewCounter(counterOpts)
+
+	if err := prometheus.Register(ev); err != nil {
+		panic(err)
+	}
+
+	return ev
+}
+
+func NewMetrics() Metrics {
+	return Metrics{
+		TotalRequests: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "total_requests",
+			Help:        "total number of requests to proxy server",
+			ConstLabels: nil,
+		}),
+		SuccessfulRequests: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "successful_requests",
+			Help:        "total number of successful requests",
+			ConstLabels: nil,
+		}),
+		FailedRequests: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "failed_requests",
+			Help:        "total number of failed requests",
+			ConstLabels: nil,
+		}),
+	}
+}
