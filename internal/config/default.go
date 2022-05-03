39bc9f18c5d86a6a64d7ff1668eb67c1b9ae8940
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 10:07:01 +0430

    default configs added

--- internal/config/default.go
+++ internal/config/default.go
@@ -1,5 +1,19 @@
 package config
 
+import (
+	"github.com/amirhnajafiz/xerox/internal/metric"
+	"github.com/amirhnajafiz/xerox/proxy"
+)
+
 func Default() Config {
-	return Config{}
+	return Config{
+		Metric: metric.Config{
+			Enable: false,
+			Host:   "",
+		},
+		Proxy: proxy.Config{
+			Address: ":8080",
+			BaseURL: "",
+		},
+	}
 }
