e7998ce5c67411663178b6f60a2e1ada8c50cb6f
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-03 09:09:38 +0430

    headers for hop proxy added

---
+++ proxy/header.go
@@ -0,0 +1,38 @@
+package proxy
+
+import (
+	"net/http"
+	"strings"
+)
+
+var headers = []string{
+	"Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te",
+	"Trailers",
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
+func CopyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func DeleteHeaders(header http.Header) {
+	for _, h := range headers {
+		header.Del(h)
+	}
+}
+
+func AppendHostToXForwardHeader(header http.Header, host string) {
+	if prior, ok := header["X-Forwarded-For"]; ok {
+		host = strings.Join(prior, ", ") + ", " + host
+	}
+	header.Set("X-Forwarded-For", host)
+}
