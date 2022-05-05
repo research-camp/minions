99aabec70ed6f23f9cf5d53e331e55cdfc0c1d07
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-05 13:45:10 +0430

    logger new logger method added

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,5 +1,30 @@
 package logger
 
-type Config struct {
-	Level string `koanf:"level"`
+import (
+	"log"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func NewLogger(cfg Config) *zap.Logger {
+	var lvl zapcore.Level
+
+	if err := lvl.Set(cfg.Level); err != nil {
+		log.Printf("cannot parse log level %s: %s", cfg.Level, err)
+
+		lvl = zapcore.WarnLevel
+	}
+
+	encoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	defaultCore := zapcore.NewCore(encoder, zapcore.Lock(zapcore.AddSync(os.Stderr)), lvl)
+	cores := []zapcore.Core{
+		defaultCore,
+	}
+
+	core := zapcore.NewTee(cores...)
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+
+	return logger
 }
