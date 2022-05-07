dffbc2f83730431fa9f72991c322af5fed861ba0
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-07 10:34:31 +0430

    proxy base url from env variable added

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -3,6 +3,7 @@ package proxy
 import (
 	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/amirhnajafiz/xerox/internal/metric"
 	"go.uber.org/zap"
@@ -11,6 +12,10 @@ import (
 // New : creates a new reverse proxy server on port 8080
 func New(logger *zap.Logger, cfg Config) {
 	// forward client to the main server
+	if bURL, ok := os.LookupEnv("BASE_URL"); ok {
+		cfg.BaseURL = bURL
+	}
+
 	originServerURL, err := url.Parse(cfg.BaseURL)
 	if err != nil {
 		logger.Fatal("invalid origin server URL")
