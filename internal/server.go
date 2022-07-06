69a4f2b398d9f4d92c20bc4a0011b6f151c58a07
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-06 10:53:51 +0430

    add: created time

--- internal/server.go
+++ internal/server.go
@@ -5,14 +5,16 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 // Server type
 type Server struct {
-	Address string
-	Target  *url.URL
+	address   string
+	target    *url.URL
+	createdAt time.Time
 
-	Handler ReqHandFunc
+	handler ReqHandFunc
 }
 
 // New creates a new reverse proxy server on port 8080
@@ -25,20 +27,21 @@ func New(target string, address string) Server {
 
 	// reverse proxy server initialize
 	return Server{
-		Address: address,
-		Target:  originServerURL,
-		Handler: HandleRequest(originServerURL),
+		address:   address,
+		target:    originServerURL,
+		createdAt: time.Now(),
+		handler:   HandleRequest(originServerURL),
 	}
 }
 
 // GetInfo returns the proxy server information
 func (s Server) GetInfo() string {
-	return fmt.Sprintf("proxy_server: %s, targeting: %s\n", s.Address, s.Target)
+	return fmt.Sprintf("[%v] proxy_server: %s, targeting: %s\n", s.createdAt, s.address, s.target)
 }
 
 // Start the proxy server
 func (s Server) Start() {
 	go func() {
-		log.Fatal(http.ListenAndServe(s.Address, http.HandlerFunc(s.Handler)))
+		log.Fatal(http.ListenAndServe(s.address, http.HandlerFunc(s.handler)))
 	}()
 }
