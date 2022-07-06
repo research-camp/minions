b5f1672e3c2897e58a127d170fec5aa9910f85ae
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-06 10:19:44 +0430

    add: proxy server initialize

--- internal/server.go
+++ internal/server.go
@@ -6,8 +6,16 @@ import (
 	"net/url"
 )
 
-// New : creates a new reverse proxy server on port 8080
-func New(target string, address string) {
+// Server type
+type Server struct {
+	Address string
+	Target  *url.URL
+
+	Handler ReqHandFunc
+}
+
+// NewProxyServer creates a new reverse proxy server on port 8080
+func NewProxyServer(target string, address string) Server {
 	// forward client to the main server
 	originServerURL, err := url.Parse(target)
 	if err != nil {
@@ -15,8 +23,16 @@ func New(target string, address string) {
 	}
 
 	// reverse proxy server initialize
-	reverseProxy := http.HandlerFunc(HandleRequest(originServerURL))
+	return Server{
+		Address: address,
+		Target:  originServerURL,
+		Handler: HandleRequest(originServerURL),
+	}
+}
 
-	// creating a new server
-	log.Fatal(http.ListenAndServe(address, reverseProxy).Error())
+// Start the proxy server
+func (s Server) Start() {
+	go func() {
+		log.Fatal(http.ListenAndServe(s.Address, http.HandlerFunc(s.Handler)))
+	}()
 }
