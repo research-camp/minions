c219a07f465aa8e6760f092f39af5e7cb26b5e35
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-06 10:25:14 +0430

    feat: interface for proxy

--- server.go
+++ server.go
@@ -1,17 +1,27 @@
-package internal
+package xerox
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/amirhnajafiz/xerox/internal"
 )
+
+// Proxy type
+type Proxy interface {
+	Start()
+
+	GetInfo() string
+}
 
 // Server type
 type Server struct {
 	Address string
 	Target  *url.URL
 
-	Handler ReqHandFunc
+	Handler internal.ReqHandFunc
 }
 
 // NewProxyServer creates a new reverse proxy server on port 8080
@@ -26,8 +36,13 @@ func NewProxyServer(target string, address string) Server {
 	return Server{
 		Address: address,
 		Target:  originServerURL,
-		Handler: HandleRequest(originServerURL),
+		Handler: internal.HandleRequest(originServerURL),
 	}
+}
+
+// GetInfo returns the proxy server information
+func (s Server) GetInfo() string {
+	return fmt.Sprintf("proxy_server: %s, targeting: %s\n", s.Address, s.Target)
 }
 
 // Start the proxy server
