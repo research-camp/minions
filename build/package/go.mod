7692835eb66274bd01ebda4145d95f556f9ecdf6
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-15 11:05:57 +0430

    add: package

---
+++ build/package/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.18
+
+require github.com/amirhnajafiz/xerox v1.0.0 // indirect
--- build/package/main.go
+++ build/package/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/amirhnajafiz/xerox" 
+import (
+	"fmt"
+	"os"
+
+	"github.com/amirhnajafiz/xerox"
+)
 
 func main() {
-   
+	var (
+		target string
+		port   = "8080"
+	)
+
+	if value, ok := os.LookupEnv("SERVER_PORT"); ok {
+		port = value
+	}
+
+	if value, ok := os.LookupEnv("SERVER_TARGET"); ok {
+		target = value
+	} else {
+		panic(fmt.Errorf("not target is set"))
+	}
+
+	proxy := xerox.NewProxyServer(target, port)
+
+	// starting the proxy server
+	proxy.Start()
 }
