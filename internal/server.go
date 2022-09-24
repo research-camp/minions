e4349c910bc5535856ef9d60c61195575c7a0f6f
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-24 14:15:14 +0330

    remove: target from server

--- internal/server.go
+++ internal/server.go
@@ -4,39 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"time"
 )
 
 // Server type
 type Server struct {
 	address   string
-	target    *url.URL
 	createdAt time.Time
 
 	handler ReqHandFunc
 }
 
 // New creates a new reverse proxy server on port 8080
-func New(target string, address string) Server {
-	// forward client to the main server
-	originServerURL, err := url.Parse(target)
-	if err != nil {
-		log.Fatal(errInvalidUrl)
-	}
-
+func New(address string) Server {
 	// reverse proxy server initialize
 	return Server{
 		address:   ":" + address,
-		target:    originServerURL,
 		createdAt: time.Now(),
-		handler:   HandleRequest(originServerURL),
+		handler:   HandleRequest(),
 	}
 }
 
 // GetInfo returns the proxy server information
 func (s Server) GetInfo() string {
-	return fmt.Sprintf("[%v] proxy_server: %s, targeting: %s\n", s.createdAt, s.address, s.target)
+	return fmt.Sprintf("[%v] proxy_server: %s\n", s.createdAt, s.address)
 }
 
 // Start the proxy server
