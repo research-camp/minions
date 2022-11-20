404bb1f0e05ba896896198a75a7d7f8b81e6f8db
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-20 16:57:26 +0330

    feat: tunnel

---
+++ tunnel.go
@@ -0,0 +1,29 @@
+package xerox
+
+import (
+	"net"
+)
+
+type SSHTunnel struct {
+	Local  *Endpoint
+	Server *Endpoint
+	Remote *Endpoint
+
+	Config *ssh.ClientConfig
+}
+
+func (tunnel *SSHTunnel) Start() error {
+	listener, err := net.Listen("tcp", tunnel.Local.String())
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go tunnel.forward(conn)
+	}
+}
