387e6feaf785510ef1b359b81a8bd548547ad34e
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-21 10:49:47 +0330

    fix: tunnel bugs

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8 // indirect
 	github.com/vishvananda/netlink v1.1.0 // indirect
 	github.com/vishvananda/netns v0.0.1 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 )
--- tunnel.go
+++ tunnel.go
@@ -1,7 +1,11 @@
 package xerox
 
 import (
+	"fmt"
+	"io"
 	"net"
+
+	"golang.org/x/crypto/ssh"
 )
 
 type SSHTunnel struct {
@@ -24,6 +28,34 @@ func (tunnel *SSHTunnel) Start() error {
 		if err != nil {
 			return err
 		}
+
 		go tunnel.forward(conn)
 	}
+}
+
+func (tunnel *SSHTunnel) forward(localConn net.Conn) {
+	serverConn, err := ssh.Dial("tcp", tunnel.Server.String(), tunnel.Config)
+	if err != nil {
+		fmt.Printf("Server dial error: %s\n", err)
+		return
+	}
+
+	remoteConn, err := serverConn.Dial("tcp", tunnel.Remote.String())
+	if err != nil {
+		fmt.Printf("Remote dial error: %s\n", err)
+		return
+	}
+
+	copyConn := func(writer, reader net.Conn) {
+		defer writer.Close()
+		defer reader.Close()
+
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			fmt.Printf("io.Copy error: %s", err)
+		}
+	}
+
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
 }
