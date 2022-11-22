6d5726685b7303eec63bb0a7064373884555620c
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-22 11:12:53 +0330

    update: ssh terminal

--- terminal.go
+++ terminal.go
@@ -1,5 +1,9 @@
 package xerox
 
+// SSHTerminal
+// to run the command on the remote machine, we should create a pseudo terminal on the remote machine.
+// A pseudo-terminal (or “pty”) is a pair of virtual character devices that provide a
+// bidirectional communication channel.
 type SSHTerminal struct {
 	Echo             uint32
 	TtyOpInputSpeed  uint32
