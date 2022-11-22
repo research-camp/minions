9434473ed4a3c1351002dd1cc65a5c0017c49b19
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-22 09:59:27 +0330

    feat: client connect method

--- client.go
+++ client.go
@@ -1,9 +1,22 @@
 package xerox
 
-import "golang.org/x/crypto/ssh"
+import (
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+)
 
 type SSHClient struct {
 	Server  *Endpoint
-	Config  *ssh.Config
+	Config  *ssh.ClientConfig
 	Session *ssh.Session
+}
+
+func (client *SSHClient) Connect() error {
+	connection, err := ssh.Dial("tcp", client.Server.String(), client.Config)
+	if err != nil {
+		return fmt.Errorf("ssh dial failed:\n\t%v\n", err)
+	}
+
+	return nil
 }
