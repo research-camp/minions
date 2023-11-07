45ac51beed8f2a6ba6ea082c9bb51eda62ef4a73
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-07 10:09:24 +0330

    add: minio init

--- internal/storage/minio.go
+++ internal/storage/minio.go
@@ -1,1 +1,24 @@
 package storage
+
+import (
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type Storage struct {
+	client *minio.Client
+}
+
+func New(cfg Config) (*Storage, error) {
+	client, err := minio.New(cfg.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(cfg.AccessKey, cfg.SecretKey, ""),
+		Secure: cfg.SSL,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Storage{
+		client: client,
+	}, nil
+}
