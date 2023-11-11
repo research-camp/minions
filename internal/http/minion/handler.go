f40ab21dd3a805265ed768a49c5ea9522a32111d
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-11 16:24:30 +0330

    init: minio handlers

--- internal/http/minion/handler.go
+++ internal/http/minion/handler.go
@@ -1,1 +1,7 @@
 package minion
+
+import "github.com/amirhnajafiz/minions/internal/storage"
+
+type Handler struct {
+	MinIO storage.Storage
+}
--- internal/http/router/handler.go
+++ internal/http/router/handler.go
@@ -1,1 +1,4 @@
 package router
+
+type Handler struct {
+}
