5d7a14156b00db72814b24bdfc89b0cecf79a4bc
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-11 16:28:26 +0330

    init: minio command

--- cmd/minion.go
+++ cmd/minion.go
@@ -1,1 +1,19 @@
 package cmd
+
+import (
+	"github.com/amirhnajafiz/minions/internal/http/minion"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Minion struct {
+}
+
+func (m Minion) main() {
+	app := fiber.New()
+
+	h := minion.Handler{}
+
+	app.Get("/download", h.Download)
+	app.Post("/upload", h.Upload)
+}
