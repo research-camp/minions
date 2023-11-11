ac22dcd73d06384e9be3f9547bce2ec44a8b16a9
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-11 16:21:36 +0330

    add: put method

--- internal/storage/config.go
+++ internal/storage/config.go
@@ -4,5 +4,6 @@ type Config struct {
 	Endpoint  string
 	AccessKey string
 	SecretKey string
+	Bucket    string
 	SSL       bool
 }
--- internal/storage/minio.go
+++ internal/storage/minio.go
@@ -1,12 +1,15 @@
 package storage
 
 import (
+	"context"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 type Storage struct {
 	client *minio.Client
+	cfg    Config
 }
 
 func New(cfg Config) (*Storage, error) {
@@ -20,11 +23,16 @@ func New(cfg Config) (*Storage, error) {
 
 	return &Storage{
 		client: client,
+		cfg:    cfg,
 	}, nil
 }
 
 func (s Storage) Put(name string, path string) error {
-	return nil
+	ctx := context.Background()
+
+	_, err := s.client.FPutObject(ctx, s.cfg.Bucket, name, path, minio.PutObjectOptions{ContentType: "application/octet-stream"})
+
+	return err
 }
 
 func (s Storage) Get(name string) (string, error) {
