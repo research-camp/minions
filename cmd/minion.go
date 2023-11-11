907b10d5a451d5ef3a7c0f5400cce214a058cabf
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-11 16:44:16 +0330

    add: cobra commands

--- cmd/minion.go
+++ cmd/minion.go
@@ -1,12 +1,24 @@
 package cmd
 
 import (
+	"github.com/amirhnajafiz/minions/internal/config"
 	"github.com/amirhnajafiz/minions/internal/http/minion"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/cobra"
 )
 
 type Minion struct {
+	Cfg config.Config
+}
+
+func (m Minion) Command() *cobra.Command {
+	return &cobra.Command{
+		Use: "minion",
+		Run: func(cmd *cobra.Command, args []string) {
+			m.main()
+		},
+	}
 }
 
 func (m Minion) main() {
--- cmd/router.go
+++ cmd/router.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"github.com/amirhnajafiz/minions/internal/config"
+
+	"github.com/spf13/cobra"
+)
+
+type Router struct {
+	Cfg config.Config
+}
+
+func (r Router) Command() *cobra.Command {
+	return &cobra.Command{
+		Use: "router",
+		Run: func(cmd *cobra.Command, args []string) {
+			r.main()
+		},
+	}
+}
+
+func (r Router) main() {
+
+}
