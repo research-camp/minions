401682d59b9eb891ad468eaca838c7a99739d7e3
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-11 16:45:41 +0330

    init: root cobra

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/amirhnajafiz/minions/cmd"
 
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	root := &cobra.Command{}
+
+	root.AddCommand(
+		cmd.Minion{}.Command(),
+		cmd.Router{}.Command(),
+	)
+
+	if err := root.Execute(); err != nil {
+		panic(err)
+	}
 }
