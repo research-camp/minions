a5b11ae9a268f3fd74b215badc0807973e2672a8
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-13 10:53:41 +0330

    add: metrics struct

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,1 +1,33 @@
 package metrics
+
+type Metrics struct {
+	caches    int
+	hits      int
+	misses    int
+	uploads   int
+	downloads int
+}
+
+func (m *Metrics) Init(caches int) {
+	m.caches = caches
+	m.hits = 0
+	m.misses = 0
+	m.uploads = 0
+	m.downloads = 0
+}
+
+func (m *Metrics) Hit() {
+	m.hits++
+}
+
+func (m *Metrics) Miss() {
+	m.misses++
+}
+
+func (m *Metrics) Up() {
+	m.uploads++
+}
+
+func (m *Metrics) Down() {
+	m.downloads++
+}
