681f824fc4c9f1ecdf5cc36ffb7cb856837afe58
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-13 11:20:26 +0330

    add: minio upload for minion

--- internal/http/minion/handler.go
+++ internal/http/minion/handler.go
@@ -1,6 +1,8 @@
 package minion
 
 import (
+	"fmt"
+
 	"github.com/amirhnajafiz/minions/internal/storage"
 
 	"github.com/gofiber/fiber/v2"
@@ -15,5 +17,20 @@ func (h Handler) Download(ctx *fiber.Ctx) error {
 }
 
 func (h Handler) Upload(ctx *fiber.Ctx) error {
-	return nil
+	form, err := ctx.MultipartForm()
+	if err != nil {
+		return fmt.Errorf("failed to get multipart form: %w", err)
+	}
+
+	for _, file := range form.File["file"] {
+		if er := ctx.SaveFile(file, fmt.Sprintf("./%s", file.Filename)); er != nil {
+			return fmt.Errorf("failed to save file on local: %w", er)
+		}
+
+		if er := h.MinIO.Put(file.Filename, file.Filename); er != nil {
+			return fmt.Errorf("failed to send on MinIo: %w", er)
+		}
+	}
+
+	return ctx.SendStatus(fiber.StatusOK)
 }
