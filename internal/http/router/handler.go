ebfaee47d6fe969a7b56a71af176294f639f540e
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-13 10:55:23 +0330

    add: metrics to handler

--- internal/http/router/handler.go
+++ internal/http/router/handler.go
@@ -2,18 +2,24 @@ package router
 
 import (
 	"github.com/amirhnajafiz/minions/internal/config"
+	"github.com/amirhnajafiz/minions/internal/metrics"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct {
-	Cfg config.RouterConfig
+	Cfg     config.RouterConfig
+	Metrics *metrics.Metrics
 }
 
 func (h Handler) Get(ctx *fiber.Ctx) error {
+	h.Metrics.Down()
+
 	return nil
 }
 
 func (h Handler) Put(ctx *fiber.Ctx) error {
+	h.Metrics.Up()
+
 	return nil
 }
