c401423e5f3f139e1100c13f8ba9234d8db487e0
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-15 11:59:05 +0330

    add: routes file for handlers

--- internal/http/minion/handler.go
+++ internal/http/minion/handler.go
@@ -1,77 +1,8 @@
 package minion
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/amirhnajafiz/minions/pkg/enum"
-
-	"github.com/gofiber/fiber/v2"
-)
-
 const LocalDir = "./tmp/local"
 
 type Handler struct {
 	Router string
 	MinIO  MinIO
-}
-
-func (h Handler) Notify(code string) {
-	url := fmt.Sprintf("%s?signal=%s", h.Router, code)
-	request, _ := http.NewRequest(fiber.MethodGet, url, nil)
-	client := http.Client{}
-
-	if _, err := client.Do(request); err != nil {
-		log.Println(fmt.Errorf("failed to send signal: %w", err))
-	}
-}
-
-func (h Handler) Download(ctx *fiber.Ctx) error {
-	name := ctx.Query("file", "")
-	if len(name) == 0 {
-		return ctx.SendStatus(fiber.StatusNotFound)
-	}
-
-	code := enum.HitSignal
-	path := fmt.Sprintf("%s/%s", LocalDir, name)
-
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
-			if er := h.MinIO.Get(name, path); er != nil {
-				return ctx.SendStatus(fiber.StatusInternalServerError)
-			}
-
-			code = enum.MissSignal
-		} else {
-			log.Println(fmt.Errorf("failed to check file: %w", err))
-
-			return ctx.SendStatus(fiber.StatusNotFound)
-		}
-	}
-
-	go h.Notify(code)
-
-	return ctx.SendFile(path)
-}
-
-func (h Handler) Upload(ctx *fiber.Ctx) error {
-	form, err := ctx.MultipartForm()
-	if err != nil {
-		return fmt.Errorf("failed to get multipart form: %w", err)
-	}
-
-	for _, file := range form.File["file"] {
-		path := fmt.Sprintf("%s/%s", LocalDir, file.Filename)
-		if er := ctx.SaveFile(file, path); er != nil {
-			return fmt.Errorf("failed to save file on local: %w", er)
-		}
-
-		if er := h.MinIO.Put(file.Filename, path); er != nil {
-			return fmt.Errorf("failed to send on MinIo: %w", er)
-		}
-	}
-
-	return ctx.SendStatus(fiber.StatusOK)
 }
--- internal/http/router/handler.go
+++ internal/http/router/handler.go
@@ -1,73 +1,8 @@
 package router
 
-import (
-	"fmt"
-
-	"github.com/amirhnajafiz/minions/internal/config"
-	"github.com/amirhnajafiz/minions/pkg/enum"
-
-	"github.com/gofiber/fiber/v2"
-)
+import "github.com/amirhnajafiz/minions/internal/config"
 
 type Handler struct {
 	Cfg     config.RouterConfig
 	Metrics Metrics
-}
-
-func (h Handler) MetricsHandler(ctx *fiber.Ctx) error {
-	return ctx.JSON(h.Metrics.Pull())
-}
-
-func (h Handler) Signal(ctx *fiber.Ctx) error {
-	key := ctx.Query("signal")
-
-	switch key {
-	case enum.HitSignal:
-		h.Metrics.Hit()
-	case enum.MissSignal:
-		h.Metrics.Miss()
-	}
-
-	return ctx.SendStatus(fiber.StatusOK)
-}
-
-func (h Handler) Get(ctx *fiber.Ctx) error {
-	name := ctx.Query("file", "")
-	if len(name) == 0 {
-		return ctx.SendStatus(fiber.StatusNotFound)
-	}
-
-	if len(h.Cfg.Minions) == 0 {
-		return fmt.Errorf("failed to get any cache")
-	}
-
-	index := len(name) % len(h.Cfg.Minions)
-	url := h.Cfg.Minions[index]
-
-	h.Metrics.Down()
-
-	return ctx.Redirect(fmt.Sprintf("%s/download", url))
-}
-
-func (h Handler) Put(ctx *fiber.Ctx) error {
-	form, err := ctx.MultipartForm()
-	if err != nil {
-		return fmt.Errorf("failed to get multipart form: %w", err)
-	}
-
-	url := ""
-
-	for _, file := range form.File["file"] {
-		index := len(file.Filename) % len(h.Cfg.Minions)
-
-		url = h.Cfg.Minions[index]
-	}
-
-	if len(url) == 0 {
-		return fmt.Errorf("failed to get any cache: %w", err)
-	}
-
-	h.Metrics.Up()
-
-	return ctx.Redirect(fmt.Sprintf("%s/upload", url))
 }
